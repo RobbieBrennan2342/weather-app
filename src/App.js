d01c29747b2307fea5705ed1675dd4a23bec85b3
robbiebrennan2342 <robertb2342@gmail.com>
2022-01-13 15:09:08 +0000

    logic for weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [weatherData, setWeatherData] = useState({});
+
+  async function fetchData() {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${process.env.REACT_APP_API_KEY}`
+    );
+    const data = await response.json();
+    console.log(data);
+    const weatherDetails = {
+      id: data.id,
+      name: data.name,
+      lat: data.coord.lat,
+      long: data.coord.lon,
+      wind: data.wind.deg,
+      main: data.main.feels_like,
+      icon: data.weather[0].icon,
+      sys: data.sys.country,
+    };
+    setWeatherData(weatherDetails);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{weatherData.id}</h1>
+      <h1>{weatherData.name}</h1>
+      <h1>{weatherData.wind}</h1>
+      <h1>{weatherData.lat}</h1>
+      <h1>{weatherData.long}</h1>
+      <h1>{weatherData.main}</h1>
+      <img src={`http://openweathermap.org/img/w/${weatherData.icon}.png`} />
+      <h1>{weatherData.sys}</h1>
     </div>
   );
 }
